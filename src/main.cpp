0178afa81f1e38203797e24574c6dc2571be7055
lolgeny <joel@swedensky.com>
2022-04-26 15:58:30 +0100

    commit before big changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "pros/vision.h"
 #include <initializer_list>
 #include <utility>
 
@@ -52,7 +53,7 @@ constexpr int32_t RED_SIG = 3;
 vision_signature_s_t YELLOW_MOGO = Vision::signature_from_utility(YELLOW_SIG, 95, 1519, 806, -3341, -2601, -2972, 2.500, 0);
 vision_signature_s_t BLUE_MOGO = Vision::signature_from_utility(BLUE_SIG, -3091, -2277, -2684, 9599, 14073, 11836, 3.000, 0);
 vision_signature_s_t RED_MOGO = Vision::signature_from_utility(RED_SIG, 4801, 6877, 5840, -65, 567, 250, 2.500, 0);
-Vision vs {9};
+Vision vs {12};
 
 /// Transform an analog joystick value into an output power (for wheels)
 int32_t process_analog(double v) {
@@ -75,11 +76,6 @@ void initialize() {
 	vs.set_signature(YELLOW_SIG, &YELLOW_MOGO);
 	vs.set_signature(BLUE_SIG, &BLUE_MOGO);
 	vs.set_signature(RED_SIG, &RED_MOGO);
-
-	// lift handling
-	hlift.move_velocity(-100);
-	delay(500);
-	hlift.move(0);
 }
 
 
@@ -88,95 +84,84 @@ void disabled() {}
 
 void competition_initialize() {
 	
-	hlift.move_velocity(-100);
-	delay(500);
-	hlift.move(0);
 }
 
-/// Run a PID loop with the given constants, to goal. Will stop when error is within thresh.
-/// `progress()` gives the current progress to determine the error;
-/// `output(double power)` should power motors necessary to achieve the goal
-void pid(
-	double kP, double kI, double kD, double goal, double thresh,
-	std::function<double()> progress, std::function<void(double)> output
-) {
-	double p, error, integral, derivative, perror;
-	do {
-		p = progress();
-		perror = error;
-		error = goal - p;
-		integral += error;
-		derivative = error - perror;
-		output(kP * error + kI * integral + kD * derivative);
-		pros::delay(5);
-	} while (error > thresh);
-}
+// /// Run a PID loop with the given constants, to goal. Will stop when error is within thresh.
+// /// `progress()` gives the current progress to determine the error;
+// /// `output(double power)` should power motors necessary to achieve the goal
+// void pid(
+// 	double kP, double kI, double kD, double goal, double thresh,
+// 	std::function<double()> progress, std::function<void(double)> output
+// ) {
+// 	double p, error, integral, derivative, perror;
+// 	do {
+// 		p = progress();
+// 		perror = error;
+// 		error = goal - p;
+// 		integral += error;
+// 		derivative = error - perror;
+// 		output(kP * error + kI * integral + kD * derivative);
+// 		pros::delay(5);
+// 	} while (error > thresh);
+// }
 
-constexpr double METRES_TO_TICKS = 1.0;
-/// Move the distance in metres - rough, so use vision or limit switch when close
-void move(int distance) {
-	pid(1.0, 1.0, 1.0, distance*METRES_TO_TICKS, 0.1, [&](){
-		return l1.get_position();
-	}, [&](double power){
-		left.move_velocity(power);
-		right.move_velocity(power);
-	});
-}
+// constexpr double METRES_TO_TICKS = 1.0;
+// /// Move the distance in metres - rough, so use vision or limit switch when close
+// void move(int distance) {
+// 	pid(1.0, 1.0, 1.0, distance*METRES_TO_TICKS, 0.1, [&](){
+// 		return l1.get_position();
+// 	}, [&](double power){
+// 		left.move_velocity(power);
+// 		right.move_velocity(power);
+// 	});
+// }
 
-/// Turn the specified angle, should be accurate due to inertial
-void turn(int deg) {
-	inert.tare();
-	pid(1.0, 1.0, 1.0, deg, 0.1, [&](){
-		return inert.get_yaw();
-	}, [&](double power){
-		left.move_velocity(power);
-		right.move_velocity(-power);
-	});
-}
+// /// Turn the specified angle, should be accurate due to inertial
+// void turn(int deg) {
+// 	inert.tare();
+// 	pid(1.0, 1.0, 1.0, deg, 0.1, [&](){
+// 		return inert.get_yaw();
+// 	}, [&](double power){
+// 		left.move_velocity(power);
+// 		right.move_velocity(-power);
+// 	});
+// }
 
-/// Balance the mogo on the ramp in front of us
-void balance_mogo() {
+// /// Balance the mogo on the ramp in front of us
+// void balance_mogo() {
 
-}
+// }
 
-/// Chase forwards until a mogo is reached (with the limit switch)
-void chase_mogo() {
-	// TODO: hook up vision sensor to stay on target
-	left.move_velocity(600);
-	right.move_velocity(600);
-	while (claw_switch.get_new_press() == 0) {
-		pros::delay(10);
-	}
-	claw.set_value(true);
-	left.move_velocity(0);
-	right.move_velocity(0);
-}
+// /// Chase forwards until a mogo is reached (with the limit switch)
+// void chase_mogo() {
+// 	// TODO: hook up vision sensor to stay on target
+// 	left.move_velocity(600);
+// 	right.move_velocity(600);
+// 	while (claw_switch.get_new_press() == 0) {
+// 		pros::delay(10);
+// 	}
+// 	claw.set_value(true);
+// 	left.move_velocity(0);
+// 	right.move_velocity(0);
+// }
 
-/// Drive the robot up the ramp, using the inertial to detect progress
-void self_ramp() {
-	left.move_velocity(300);
-	right.move_velocity(300);
-	while (abs(inert.get_pitch()) > 10) {pros::delay(5);}
-	left.move_velocity(-100);
-	right.move_velocity(-100);
-	pros::delay(300);
-	left.move_velocity(0);
-	right.move_velocity(0);
-}
+// /// Drive the robot up the ramp, using the inertial to detect progress
+// void self_ramp() {
+// 	left.move_velocity(300);
+// 	right.move_velocity(300);
+// 	while (abs(inert.get_pitch()) > 10) {pros::delay(5);}
+// 	left.move_velocity(-100);
+// 	right.move_velocity(-100);
+// 	pros::delay(300);
+// 	left.move_velocity(0);
+// 	right.move_velocity(0);
+// }
+
 
 constexpr double INERT_FACTOR = 0.1;
-
-/// Runs the autonomous routine
-// TODO: better system than downloading twice for skills vs match
-void autonomous() {
-	left.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-	right.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-
-	// inert.tare();
-	
-	// hlift.move_velocity(-100);
-	double time = 0;
+void chase_mogo() {
 	mogo_block.set_value(true);
+	double time = 0;
 	while (!claw_switch.get_new_press() && time < 1500) {
 		double i = inert.get_heading() * INERT_FACTOR;
 		i = 0; // remove for etsting
@@ -186,29 +171,118 @@ void autonomous() {
 		delay(5);
 	}
 	claw.set_value(true);
-	// left.move_velocity(0);
-	// right.move_velocity(0);
 	delay(50);
+}
+
+void retract_and_deposit() {
+	//retracr
 	left.move_velocity(-600);
 	right.move_velocity(-600);
 	// hlift.move(0);
 	delay(1000);
-	left.move_velocity(-50);
-	right.move_velocity(50);
-	while (true) {
-		auto target = vs.get_by_size(0);
-		printf("%d middle %d errno is %d\n", target.signature, target.x_middle_coord, errno);
-		if (138 < target.x_middle_coord && target.x_middle_coord < 178) break;
-		delay(5);
-		// break;
-	}
+	// deposit
+	inert.tare();
 	left.move_velocity(-300);
-	right.move_velocity(-300);
-	delay(1000);
-	blift.set_value(true);
-	intake.move_velocity(600);
-	delay(2000);
-	intake.move_velocity(0);
+	right.move_velocity(300);
+	bool deposited = false;
+	while (inert.get_rotation() < 360) {
+		if (!deposited && inert.get_rotation() > 180) claw.set_value(false);
+		delay(10);
+	}
+	left.move_velocity(0);
+	right.move_velocity(0);
+}
+
+constexpr int VS_MID = 128;
+
+bool find_middle_mogo() {
+	while (true) {
+		auto target = vs.get_by_sig(0, YELLOW_SIG);
+		if (target.x_middle_coord == PROS_ERR) return false;
+		if (target.x_middle_coord < VS_MID - 20) {
+			// turn right
+			printf("mid is %d, going RIGHT\n", target.x_middle_coord);
+			left.move_velocity(100);
+			right.move_velocity(-100);
+		} else if (target.x_middle_coord > VS_MID + 20) {
+			// turn left
+			printf("mid is %d, going LEFT\n", target.x_middle_coord);
+			left.move_velocity(-100);
+			right.move_velocity(100);
+		} else {
+			left.move_velocity(0);
+			right.move_velocity(0);
+			return true;
+		}
+		// 	left.move_velocity(0);
+		// 	right.move_velocity(0);
+		// return true;
+		delay(5);
+	}
+}
+
+/// Runs the autonomous routine
+// TODO: better system than downloading twice for skills vs match
+void autonomous_old() {
+	left.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+	right.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+
+	// inert.tare();
+	
+	// hlift.move_velocity(-100);
+
+	// chase_mogo();
+	// retract_and_deposit();
+
+	// while (find_middle_mogo()) {
+	// 	chase_mogo();
+	// 	retract_and_deposit();
+	// }
+	find_middle_mogo();
+
+	// left.move_velocity(-50);
+	// right.move_velocity(50);
+	// while (true) {
+	// 	auto target = vs.get_by_size(0);
+	// 	printf("%d middle %d errno is %d\n", target.signature, target.x_middle_coord, errno);
+	// 	if (138 < target.x_middle_coord && target.x_middle_coord < 178) break;
+	// 	delay(5);
+	// 	// break;
+	// }
+	// left.move_velocity(-300);
+	// right.move_velocity(-300);
+	// delay(1000);
+	// blift.set_value(true);
+	// intake.move_velocity(600);
+	// delay(2000);
+	// intake.move_velocity(0);
+	left.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	right.set_brake_mode(E_MOTOR_BRAKE_COAST);
+}
+
+class PID {
+private:
+	const double kP, kI, kD;
+	double error = 0, derivative = 0, integral = 0;
+public:
+	double target = 0;
+	PID(double kP, double kI, double kD): kP{kP}, kI{kI}, kD{kD} {}
+	double update(double sensor) {
+		double new_error = target - sensor;
+		derivative = new_error - error;
+		error = new_error;
+		integral += error;
+		return kP * error + kI * integral + kD * derivative;
+	}
+};
+
+void autonomous() {
+	left.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+	right.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+
+	PID drivePID {1.0, 0.0, 0.0};
+	PID turnPID {1.0, 0.0, 0.0};
+
 	left.set_brake_mode(E_MOTOR_BRAKE_COAST);
 	right.set_brake_mode(E_MOTOR_BRAKE_COAST);
 }
